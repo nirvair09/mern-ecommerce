a37f975525bb40390bafb4247660e21a260645af
nirvair09 <chandradeep262003@gmail.com>
2025-12-19 12:01:34 +0530

    feat: initialize MERN e-commerce backend server, configure PayPal SDK, and add client .env to gitignore.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -19,7 +20,7 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("db_url")
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
